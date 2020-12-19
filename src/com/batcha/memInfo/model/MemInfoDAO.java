1531c5d9350834735342e5479a2929e22d43c960
ebn427 <67459851+ebn427@users.noreply.github.com>
2020-12-20 03:57:40 +0900

    update memInfo(dao, vo, service)

--- src/com/batcha/memInfo/model/MemInfoDAO.java
+++ src/com/batcha/memInfo/model/MemInfoDAO.java
@@ -1,5 +1,98 @@
 package com.batcha.memInfo.model;
 
-public class MemInfoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 
+import com.batcha.db.ConnectionPoolMgr2;
+import com.mystudy.member.model.MemberVO;
+
+public class MemInfoDAO {
+	private ConnectionPoolMgr2 pool;
+	
+	public MemInfoDAO() {
+		pool=new ConnectionPoolMgr2();
+	}
+	
+	public int loginCheck(String userid, String pwd) throws SQLException {
+		Connection con=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		int result=0;
+		
+		try {
+			con=pool.getConnection();
+			
+			String sql="select pwd from memInfo" + 
+					" where id=? and outdate is null";
+			ps=con.prepareStatement(sql);
+			ps.setString(1, userid);
+			
+			rs=ps.executeQuery();
+			if(rs.next()) {
+				String dbPwd=rs.getString(1);
+				if(dbPwd.equals(pwd)) {
+					result=MemInfoService.LOGIN_OK;
+				}else {
+					result=MemInfoService.PWD_DISAGREE;
+				}
+			}else {
+				result=MemInfoService.ID_NONE;
+			}
+			
+			System.out.println("로그인 처리 결과, result="+result
+				+", 매개변수 userid="+userid+", pwd="+pwd);
+			return result;
+		}finally {
+			pool.dbClose(rs, ps, con);
+		}
+	}//
+	
+	public MemInfoVO selectMember(String userid) throws SQLException {
+		Connection con=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		
+		MemInfoVO vo = new MemInfoVO();
+		try {
+			con=pool.getConnection();
+			
+			String sql="select * from memInfo where id=?";
+			ps=con.prepareStatement(sql);
+			ps.setString(1, userid);
+			
+			rs=ps.executeQuery();
+			if(rs.next()) {
+				int memNo=rs.getInt("memno");
+				String id=rs.getString("id");
+				String pwd=rs.getString("pwd");
+				String name=rs.getString("name");
+				String email=rs.getString("email");
+				String mobile=rs.getString("mobile");
+				String birthday=rs.getString("birthday");
+				Timestamp regdate=rs.getTimestamp("regdate");
+				Timestamp outdate=rs.getTimestamp("outdate");
+				int adminCheck=rs.getInt("admincheck");
+				
+				vo.setMemNo(memNo);
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setMobile(mobile);
+				vo.setBirthday(birthday);
+				vo.setRegdate(regdate);
+				vo.setOutdate(outdate);
+				vo.setAdminCheck(adminCheck);
+			}
+			System.out.println("회원정보 조회 결과 vo="+vo+", 매개변수 userid="
+					+userid);
+			
+			return vo;
+		}finally {
+			pool.dbClose(rs, ps, con);
+		}
+	}//
 }
--- src/com/batcha/memInfo/model/MemInfoService.java
+++ src/com/batcha/memInfo/model/MemInfoService.java
@@ -1,5 +1,25 @@
 package com.batcha.memInfo.model;
 
-public class MemInfoService {
+import java.sql.SQLException;
 
+public class MemInfoService {
+	
+	//로그인 처리시 사용
+	public static final int LOGIN_OK=1; //로그인 성공
+	public static final int ID_NONE=2; //아이디가 없는 경우
+	public static final int PWD_DISAGREE=3; //비밀번호 불일치
+	
+	private MemInfoDAO memInfoDao;
+	
+	public MemInfoService() {
+		memInfoDao=new MemInfoDAO();
+	}
+	
+	public int loginCheck(String userid, String pwd) throws SQLException{
+		return memInfoDao.loginCheck(userid, pwd);
+	}
+	
+	public MemInfoVO selectMember(String userid) throws SQLException{
+		return memInfoDao.selectMember(userid);
+	}
 }
